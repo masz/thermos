1abb94228342f13d8870035a493be1562f4b611a
masz <masz@mail.de>
2019-06-02 19:07:43 +0200

    python3 adjustments

--- manage.py
+++ manage.py
@@ -12,14 +12,14 @@ def initdb():
     db.create_all()
     db.session.add(User(username="mickey", email="mickey@mouse.com"))
     db.session.commit()
-    print 'Initialized the database'
+    print('Initialized the database')
 
 
 @manager.command
 def dropdb():
     if prompt_bool("Are you sure you want to lose all your data"):
         db.drop_all()
-        print 'Dropped the database'
+        print('Dropped the database')
 
 
 if __name__ == '__main__':
--- thermos/views.py
+++ thermos/views.py
@@ -1,13 +1,13 @@
 from flask import render_template, flash, redirect, url_for
 
 from thermos import app, db
-from forms import BookmarkForm
-from models import User, Bookmark
+from thermos.forms import BookmarkForm
+from thermos.models import User, Bookmark
 
 
 # fake login
 def logged_in_user():
-    return User.query.filter_by(username="Mickey").first()
+    return User.query.filter_by(username="mickey").first()
 
 
 @app.route('/')
@@ -22,12 +22,18 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        bm = Bookmark(url=url, description=description)
+        bm = Bookmark(user=logged_in_user(), url=url, description=description)
         db.session.add(bm)
         db.session.commit()
         flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
+
+
+@app.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
 
 
 @app.errorhandler(404)
