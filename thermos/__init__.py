6268aa500f260536d6a44d448cebfdce011dcd1b
utima <utima@mail.de>
2019-06-22 20:49:59 +0200

    bookmarks edit

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,6 +18,8 @@ login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.login_view = "login"
 login_manager.init_app(app)
+
+moment = Moment(app)
 
 import thermos.models
 import thermos.views
